7c49bf6bfa64ede1747184476e9bd373db169b09
pzqu <pzqu@tencent.com>
2020-06-26 12:29:48 +0800

    [+]travis update

--- .travis-push.sh
+++ .travis-push.sh
@@ -8,19 +8,24 @@ GH_REPO=https://github.com/pzqu/playdb-book.git
 
 KY_REPO_URL=https://${KANCLOUD_USER}:${KANCLOUD_PASS}@$(echo $KY_REPO | awk -F'//' '{print $2}')
 GH_REPO_URL=https://${GITHUB_TOKEN}@$(echo $GH_REPO | awk -F'//' '{print $2}')
+KY_REPO_NAME=$(echo $KY_REPO | awk -F'/' '{print $NF}' | awk -F '.' '{print $1}')
+DEST_REPO_URL=$GH_REPO_URL
+SRC_REPO_URL=$KY_REPO_URL
 
 setup_git() {
+  git show -s --format=%ct
   git config --global user.email "pzqu@qq.com"
   git config --global user.name "pzqu"
   rm -rf *
-  git clone $KY_REPO_URL
+  git clone --depth=50 --branch=master $SRC_REPO_URL
   repo_dir=$(ls) && cp -rf $repo_dir/* ./ &&  rm -rf $repo_dir
 }
 
 commit_country_json_files() {
+  git status
   git checkout master
   # Current month and year, e.g: Apr 2018
-  dateAndMonth=`date "+%b %Y"`
+  dateAndMonth= `date "+%b %Y"`
   # Stage the modified files in dist/output
   git add -A
   # Create a new commit with a custom build message
@@ -33,9 +38,24 @@ upload_files() {
   # Remove existing "origin"
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
-  git remote add origin $GH_REPO_URL > /dev/null 2>&1
+  git remote add origin $DEST_REPO_URL > /dev/null 2>&1
   git push origin master --quiet
 }
+
+compare_new() {
+   github_last_commit_time=$(git show -s --format=%ct)
+   git clone $KY_REPO_URL && cd $KY_REPO_NAME
+   ky_last_commit_time=$(git show -s --format=%ct)
+   if [ $github_last_commit_time -gt $ky_last_commit_time ];then
+       SRC_REPO_URL=$GH_REPO_URL
+       DEST_REPO_URL=$KY_REPO_URL
+   else
+       cd ..
+   fi
+   echo "sync $SRC_REPO_URL to $DEST_REPO_URL"
+}
+
+compare_new
 
 setup_git
 
